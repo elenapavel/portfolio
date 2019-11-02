e751482162ab8df5664ce4382bfc1ecb52a021c4
Elena Pavel <elena.pavel.dev@gmail.com>
2019-11-02 15:53:51 +0200

    Removed unused components and views.

--- source/components/Document/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import render from "./render.js";
-import $, { css } from "./style.css";
-
-export default function Document(props) {
-	return <b class={$.document}>{render(props.data)}</b>;
-}
--- source/components/Form/index.js
+++
@@ -1,5 +0,0 @@
-import useForm from "./useForm.js";
-import useField from "./useField.js";
-import * as schema from "./schema.js";
-
-export { useForm, useField, schema };
